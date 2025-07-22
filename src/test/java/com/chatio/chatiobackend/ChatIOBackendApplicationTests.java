36ff40a4bf25a533787291732871a520f4ee6963
martin.mitev <martin.mitev@sciant.com>
2025-07-22 17:04:27 +0300

    first commit

---
+++ src/test/java/com/chatio/chatiobackend/ChatIOBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatio.chatiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatIOBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
